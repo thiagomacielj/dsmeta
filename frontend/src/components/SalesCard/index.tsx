24d2635131c41c4040cb1e66144602a1e34f2323
thiagomacielj <thiagomacielj@gmail.com>
2022-07-16 15:31:06 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sales";
+import { BASE_URL } from "../../utils/request";
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 
@@ -13,10 +15,12 @@ function SalesCard() {
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
 
+  const[sales, setSales] = useState<Sale[]>([]);
+
   useEffect(()=> {
-    axios.get("http://localhost:8080/sales")
+    axios.get(`${BASE_URL}/sales`)
     .then(response =>{
-      console.log(response.data);
+      setSales(response.data.content);
     });
   }, []);
 
@@ -58,47 +62,25 @@ function SalesCard() {
         </thead>
 
         <tbody>
-          <tr>
-            <td className="show992">#341</td>
-            <td className="show576">27/07/2022</td>
-            <td>Laurister Fagundes</td>
-            <td className="show992">15</td>
-            <td className="show992">11</td>
-            <td>R$ 55300.00</td>
+
+          {sales.map(sale => {
+            return(
+              <tr key={sale.id}>
+            <td className="show992">{sale.id}</td>
+            <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+            <td>{sale.sellerName}</td>
+            <td className="show992">{sale.visited}</td>
+            <td className="show992">{sale.deals}</td>
+            <td>{sale.amount.toFixed(2)}</td>
             <td>
               <div className="dsmeta-red-btn-container">               
                   <NotificationButton/>                
               </div>
             </td>
           </tr>
-
-          <tr>
-            <td className="show992">#341</td>
-            <td className="show576">27/07/2022</td>
-            <td>João Lopes</td>
-            <td className="show992">30</td>
-            <td className="show992">13</td>
-            <td>R$ 55300.00</td>
-            <td>
-              <div className="dsmeta-red-btn-container">                
-                <NotificationButton/>                
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td className="show992">#341</td>
-            <td className="show576">27/07/2022</td>
-            <td>Glória Maria</td>
-            <td className="show992">25</td>
-            <td className="show992">21</td>
-            <td>R$ 55300.00</td>
-            <td>
-              <div className="dsmeta-red-btn-container">                
-                <NotificationButton/>                
-              </div>
-            </td>
-          </tr>
+            )
+          })}
+          
         </tbody>
       </table>
     </div>
